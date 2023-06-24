9e08fefa93687b9818b767f69f825cbdff7a9f0b
UO-JaF <127138825+UO-JaF@users.noreply.github.com>
2023-06-24 00:20:26 -0400

    Add files via upload

---
+++ Jaf's Hideshit v1.0.js
@@ -0,0 +1,13 @@
+// JustanotherFace (JaF)'s Hide Shit (people, blue people specifically, along with cusidhes, boras, and a cpl other things) Script
+// Ver: 1.0  Date: 06/18/2023 
+function JaF_hideshit() {
+while (!Player.Dead()){
+ target = Orion.FindType('0x0191|0x025E|0x029A|0x02C1|0x029B|0x0190|0x025D|0x02EB|0x02EC|0x02CB|0x0115|0x0317|0x060B|0x00B3|0x007A|0x011D|0x02D0', '-1', ground, 'mobile|live|ignorefriends|ignoreself|inlos', 20, 'blue')
+        if (target.length > 0 ) {
+            for (var i = 0; i < target.length; i++) {
+                Orion.Hide(target[i]);
+				Orion.Wait(400)
+				}
+			}
+		}
+}
